bc87f8069d465abbc6d537f6ee1860cf970497fd
Harsh517-tech415 <harshchoudharnv@gmail.com>
2023-01-31 05:39:17 +0530

    search

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
-
+import {getAuth} from 'firebase/auth'
+// import 'firebase/auth'
 const firebaseConfig = {
   apiKey: "AIzaSyDepg4IqFiqSDjrotC9Qc49trnIBl8gMKI",
   authDomain: "fitfinder-595d4.firebaseapp.com",
@@ -16,3 +17,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app  =initializeApp(firebaseConfig);
 export const db=getFirestore(app);
+export const auth=getAuth();
