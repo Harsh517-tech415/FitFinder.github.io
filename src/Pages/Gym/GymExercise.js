f5338425dc41152018f5953459746ef740f68ff8
Harsh517-tech415 <83271527+Harsh517-tech415@users.noreply.github.com>
2023-03-08 20:56:46 +0530

    gymExerciseLast1

--- src/Pages/Gym/GymExercise.js
+++ src/Pages/Gym/GymExercise.js
@@ -1,4 +1,4 @@
-import { Box, Typography, Stack, Button} from "@mui/material";
+import { Box, Typography, Stack, Button, Popover } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -11,16 +11,19 @@ import SkipNextRoundedIcon from "@mui/icons-material/SkipNextRounded";
 import { UserC } from "../../components/FitFinderInfo";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "../../components/firebase";
-import { motion,useDragControls } from "framer-motion";
+import { motion, useDragControls } from "framer-motion";
 import { CameraAlt } from "@mui/icons-material";
-import AccessibilityNewIcon from "@mui/icons-material/AccessibilityNew";
+import PersonIcon from "@mui/icons-material/Person";
 import { getDownloadURL, ref } from "firebase/storage";
-import {storage} from '../../components/firebase'
+import { storage } from "../../components/firebase";
 import Webcam from "react-webcam";
+import Slider from "@mui/material/Slider";
+import PersonOffIcon from "@mui/icons-material/PersonOff";
+import EditIcon from "@mui/icons-material/Edit";
 
 let Cookies = require("js-cookie");
 const GymExercise = ({ pathIndex }) => {
-  const controls=useDragControls()
+  const controls = useDragControls();
   const navigate = useNavigate();
   const [animationActive, setAnimationActive] = useState(null);
   let userc = new UserC();
@@ -36,7 +39,20 @@ const GymExercise = ({ pathIndex }) => {
   const second = useRef(0);
   const minute = useRef(0);
   const time = useRef(0);
-  const [gif,setGif]=useState()
+  const [gif, setGif] = useState();
+  const [recivedGif, setDisplayGif] = useState("ok");
+  const [opacityValue, setOpacityValue] = useState(0.5);
+  const [gifHeight, setGifHeight] = useState(600);
+  const [gifWidth, setGifWidth] = useState(600);
+  const [anchorEl, setAnchorEl] = useState(0);
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+  
   async function getData() {
     try {
       const aa = await getDoc(doc(db, "Exercise", Cookies.get("_adu")));
@@ -54,9 +70,9 @@ const GymExercise = ({ pathIndex }) => {
     if (fetch === 0) {
       getData();
     }
-    Cookies.get("time") === "NaN" || "NAN"
-      ? (t = 0)
-      : (t = Cookies.get("time"));
+    Cookies.get("time") === "NaN" || "NAN"||undefined
+      ? (document.cookie=`StarTime=${[new Date().getHours(),new Date().getMinutes(),new Date().getSeconds()]}; path=/gym`)
+      : (Cookies.get("time"));
     // a = setInterval(()=>{setTime()}, 1000);
 
     // document.cookie="time=22;expires=Thu, 20 Jan 2023 12:00:00 UTC;path=/gym/gymexercise"
@@ -75,7 +91,7 @@ const GymExercise = ({ pathIndex }) => {
 
   useEffect(() => {
     function handleTime() {
-      if (option === 1) {
+      if (index === exerciseDetail.length-1) {
         if (time.current === 0) {
           setOption(0);
           window.clearInterval(d);
@@ -101,18 +117,29 @@ const GymExercise = ({ pathIndex }) => {
   }, [option]);
 
   async function displayGif() {
-    try{
-      await setDoc(doc(db,"Gif","Orignal"),{gifUrl:exerciseDetail[index].gifUrl})
-      await setDoc(doc(db,"Gif","User"),{User:Cookies.get("_hash")})
-      setTimeout(()=>{
-        const starsRef=ref(storage,`${Cookies.get("_hash")}`)
-   getDownloadURL(starsRef).then((url)=>{setGif(url)}).catch((err)=>{console.log(err)})
-      },21000)
-
-    }catch(err){console.log(err)}
-   
+    try {
+      await setDoc(doc(db, "Gif", "Orignal"), {
+        gifUrl: exerciseDetail[index].gifUrl,
+      });
+      await setDoc(doc(db, "Gif", "User"), { User: Cookies.get("_hash") });
+      setTimeout(() => {
+        const starsRef = ref(storage, `${Cookies.get("_hash")}`);
+        getDownloadURL(starsRef)
+          .then((url) => {
+            console.log("ok");
+            setGif(url);
+            console.log(gif);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }, 30000);
+    } catch (err) {
+      console.log(err);
     }
-   
+    // setDisplayGif("ok")
+  }
+
   const variants = {
     displayButton: { opacity: 1 },
     closeButton: { opacity: 0 },
@@ -134,9 +161,8 @@ const GymExercise = ({ pathIndex }) => {
       setDisplayCamera("none");
     }
   }, [animationActive]);
-  function startDrag(event)
-  {
-    controls.start(event)
+  function startDrag(event) {
+    controls.start(event);
   }
   return (
     <Stack direction="row">
@@ -166,32 +192,179 @@ const GymExercise = ({ pathIndex }) => {
       </Button> */}
       {option === 0 ? (
         <Stack
-        direction="row"
-        sx={{
-          m: { sm: "1% 0% 0% 13%", lg: "1% 0% 0% 7%" },
-          width: { sm: "660px", lg: "1300px" },
-          height: { sm: "760px", lg: "750px" },
-        }}
+          direction="row"
+          sx={{
+            m: { sm: "1% 0% 0% 13%", lg: "1% 0% 0% 7%" },
+            width: { sm: "660px", lg: "1300px" },
+            height: { sm: "760px", lg: "750px" },
+          }}
         >
-
-          <Button
-            component={motion.button}
-            variants={variants}
-            initial={{ opacity: 0 }}
-            animate={animationActive ? "displayButton" : "hideButton"}
-            sx={{ m: "0% 0% 40% 0%" }}
-            onClick={displayGif}
+          <Stack
+            direction="column"
+            spacing={4}
+            sx={{ m: "20px 10px 0px 0px " }}
           >
-            <AccessibilityNewIcon
-              fontSize="large"
+            <Button
+              component={motion.button}
+              variants={variants}
+              initial={{ opacity: 0 }}
+              animate={animationActive ? "displayButton" : "hideButton"}
+              onClick={displayGif}
+              variant="contained"
               color="error"
               sx={{
-                m: "0% 0% 0% 0%",
-                borderRadius: "10px",
                 backgroundColor: "grey",
               }}
-            />
-          </Button>
+            >
+              <PersonIcon />
+            </Button>
+
+            <Button
+              component={motion.button}
+              variants={variants}
+              initial={{ opacity: 0 }}
+              animate={animationActive ? "displayButton" : "hideButton"}
+              onClick={() => {
+                setDisplayGif("none");
+              }}
+              color="error"
+              variant="contained"
+              sx={{ backgroundColor: "grey" }}
+            >
+              <PersonOffIcon variant="contained" />
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              sx={{ backgroundColor: "grey" }}
+              component={motion.button}
+              variants={variants}
+              initial={{ opacity: 0 }}
+              animate={animationActive ? "displayButton" : "hideButton"}
+              onClick={(e) => {
+                setAnchorEl(e.currentTarget);
+                console.log(anchorEl);
+              }}
+            >
+              <EditIcon />
+            </Button>
+            <Popover
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+            >
+              <Card sx={{ width: "400px" }}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    p: "0 20px",
+                  }}
+                >
+                  <Typography sx={{ fontSize: "20px", mr: "10px" }}>
+                    Opacity:
+                  </Typography>
+                  <Slider
+                    step={0.1}
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                    component={motion.div}
+                    variants={variants}
+                    initial={{ opacity: 0 }}
+                    animate={animationActive ? "displayButton" : "hideButton"}
+                    color="error"
+                    max={1}
+                    min={0}
+                    defaultValue={opacityValue}
+                    valueLabelDisplay="auto"
+                    onChange={(e) => {
+                      setOpacityValue(
+                        e.target.value === "" ? "" : Number(e.target.value)
+                      );
+                    }}
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    p: "0 20px",
+                  }}
+                >
+                  <Typography sx={{ fontSize: "20px", mr: "20px" }}>
+                    Height:
+                  </Typography>
+                  <Slider
+                    step={1}
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                    component={motion.div}
+                    variants={variants}
+                    initial={{ opacity: 0 }}
+                    animate={animationActive ? "displayButton" : "hideButton"}
+                    color="error"
+                    max={1000}
+                    min={600}
+                    defaultValue={gifHeight}
+                    valueLabelDisplay="auto"
+                    onChange={(e) => {
+                      setGifHeight(
+                        e.target.value === "" ? "" : Number(e.target.value)
+                      );
+                    }}
+                  />
+                </Box>
+
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    p: "0 20px",
+                  }}
+                >
+                  <Typography sx={{ fontSize: "20px", mr: "25px" }}>
+                    Width:
+                  </Typography>
+                  <Slider
+                    step={1}
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                    component={motion.div}
+                    variants={variants}
+                    initial={{ opacity: 0 }}
+                    animate={animationActive ? "displayButton" : "hideButton"}
+                    color="error"
+                    max={1000}
+                    min={100}
+                    defaultValue={gifWidth}
+                    valueLabelDisplay="auto"
+                    onChange={(e) => {
+                      setGifWidth(
+                        e.target.value === "" ? "" : Number(e.target.value)
+                      );
+                    }}
+                  />
+                </Box>
+              </Card>
+            </Popover>
+          </Stack>
           <Box>
             <Card
               component={motion.div}
@@ -208,11 +381,36 @@ const GymExercise = ({ pathIndex }) => {
                 boxShadow: "0px 0px 11px 2px grey",
               }}
             >
-              <Webcam ref={camera} style={{position:"absolute",zIndex:9,height:"100%"}} mirrored={true} imageSmoothing={true} screenshotFormat='image/webp' />
-              <div onPointerDown={startDrag}/>
-              {/* <Box drag component={motion.div}> */}
-              <CardMedia  sx={{position:"absolute",zIndex:10,width:"600px"}} component="img" src={gif}/>
-              {/* </Box> */}
+              <Webcam
+                ref={camera}
+                style={{ position: "absolute", zIndex:9 , height: "100%" }}
+                mirrored={true}
+                imageSmoothing={true}
+                screenshotFormat="image/webp"
+              />
+
+              <Box
+                component={motion.div}
+                drag
+                dragElastic={0}
+                sx={{
+                  position: "absolute",
+                  width: `${gifWidth}px`,
+                  height: `${gifHeight}px`,
+                  zIndex: 10,
+                }}
+                >
+                <CardMedia
+                  sx={{
+                    opacity: opacityValue,
+                    display: recivedGif,
+                    width: "100%",
+                    height: "100%",
+                  }}
+                  component="img"
+                  src={gif}
+                />
+              </Box>
             </Card>
             <Card
               sx={{
@@ -304,6 +502,22 @@ const GymExercise = ({ pathIndex }) => {
                     borderRadius: "30px",
                     m: { sm: "3% 36%", lg: "3% 39.5%" },
                   }}
+                  onClick={() => {
+                    setGif(null);
+                    if (index !== exerciseDetail.length - 1) {
+                      setOption(1);
+                      setIndex(index + 1);}
+                    else if(index===exerciseDetail.length-1)
+                    {
+                      let getTime=Cookies.get("StarTime").split(",")
+                      const currentTime=[new Date().getHours(),new Date().getMinutes(),new Date().getSeconds()]
+                      let getTimeMinute=Math.floor([(getTime[0]*60*60)+(getTime[1]*60)+Number(getTime[2])]/60)
+                      let getTimeSecond=[(getTime[0]*60*60)+(getTime[1]*60)+Number(getTime[2])]%60
+                      let currentTimeMinute=Math.floor([(currentTime[0]*60*60)+(currentTime[1]*60)+Number(currentTime[2])]/60)
+                      let currentTimeSecond=[(currentTime[0]*60*60)+(currentTime[1]*60)+Number(currentTime[2])]%60
+                      console.log("Minute:",currentTimeMinute-getTimeMinute,"Second:",Math.abs(currentTimeSecond-getTimeSecond))
+                    navigate('/gym/gymresult')}
+                  }}
                 >
                   Done
                 </Button>
@@ -322,6 +536,7 @@ const GymExercise = ({ pathIndex }) => {
                 <Button
                   color="error"
                   onClick={() => {
+                    setGif(null);
                     if (index !== exerciseDetail.length - 1) {
                       setOption(1);
                       setIndex(index + 1);
